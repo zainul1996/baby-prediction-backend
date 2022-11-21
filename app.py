5b7182611b445ac540455f4076c9dbeca61e2615
ZainulCain <zainulcain@ZainulCains-MacBook-Pro.local>
2022-11-22 00:52:49 +0800

    changed default port to 5000

--- app.py
+++ app.py
@@ -361,4 +361,4 @@ def index():
 
 if __name__ == "__main__":
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5001, debug=True)
+    app.run(threaded=True, debug=True)
